16fab41a837b9ce90bc1ba19c0acbf120e087b7e
DaryaZzz <kislorodo2@outlook.com>
2024-06-11 20:57:12 +0300

    Unique usernames.

--- app/src/main/java/com/example/server_test/MainActivity.java
+++ app/src/main/java/com/example/server_test/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.server_test;
 
+import static java.lang.System.exit;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -18,32 +20,29 @@ import com.example.server_test.model.User;
 import com.example.server_test.retrofit.RetrofitService;
 import com.example.server_test.retrofit.UserApi;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import okhttp3.CacheControl;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    TextView lastnameF;
-    String usernameS, firstnameS;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lastnameF = (TextView) findViewById(R.id.lastnameF);
     }
 
 
-    public void sendPOST(View view) {
+    public void sign_in(View view) {
         RetrofitService retrofitService = new RetrofitService();
         UserApi userApi = retrofitService.getRetrofit().create(UserApi.class);
 
@@ -59,21 +58,78 @@ public class MainActivity extends AppCompatActivity {
         userApi.saveUser(user).enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
-                Toast.makeText(MainActivity.this, "Save is successful!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "You've been registered!", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this, Shelf.class);
                 startActivity(intent);
             }
 
             @Override
             public void onFailure(Call<User> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Save failed!!!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "The registration failed", Toast.LENGTH_SHORT).show();
                 Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, "Error occured", t);
+            }
+        });
+
+    }
+
+
+    public void sign_up(View view) {
+        RetrofitService retrofitService = new RetrofitService();
+        UserApi userApi = retrofitService.getRetrofit().create(UserApi.class);
+
+        EditText usernameET = (EditText) findViewById(R.id.username);
+        EditText passwordET = (EditText) findViewById(R.id.password);
+        String username = usernameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+
+        userApi.getAllUsers().enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                List<User> list = response.body();
+
+                for (User user1: list){
+                    if(user1.getUsername().equals(username)) {
+                        Toast.makeText(MainActivity.this, "Username already exists, choose another one.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+
+                sendPOST(userApi, username, password);
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Taking list failed.", Toast.LENGTH_SHORT).show();
+                return;
             }
         });
 
 
 
 
+    }
+
+    public void sendPOST(UserApi userApi, String username, String password){
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+
+
+        userApi.saveUser(user).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                Toast.makeText(MainActivity.this, "You've been registered!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, Shelf.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "The registration failed", Toast.LENGTH_SHORT).show();
+                Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, "Error occured", t);
+            }
+        });
     }
 
 }
