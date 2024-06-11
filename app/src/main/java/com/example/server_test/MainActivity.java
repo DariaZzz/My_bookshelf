52b041474039945565e4b7dddc9aef392a7f3cfe
DaryaZzz <kislorodo2@outlook.com>
2024-06-11 22:06:01 +0300

    Login implemented.

--- app/src/main/java/com/example/server_test/MainActivity.java
+++ app/src/main/java/com/example/server_test/MainActivity.java
@@ -51,27 +51,30 @@ public class MainActivity extends AppCompatActivity {
         String username = usernameET.getText().toString();
         String password = passwordET.getText().toString();
 
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
-
-        userApi.saveUser(user).enqueue(new Callback<User>() {
+        userApi.getAllUsers().enqueue(new Callback<List<User>>() {
             @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                Toast.makeText(MainActivity.this, "You've been registered!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, Shelf.class);
-                startActivity(intent);
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+
+                for (User user1: response.body()){
+                    if(user1.getUsername().equals(username) && user1.getPassword().equals(password)) {
+                        Intent intent = new Intent(MainActivity.this, Shelf.class);
+                        intent.putExtra("id", user1.getId());
+                        startActivity(intent);
+                        return;
+                    }
+                }
+
+                Toast.makeText(MainActivity.this, "Wrong login or password.", Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "The registration failed", Toast.LENGTH_SHORT).show();
-                Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, "Error occured", t);
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Taking list failed.", Toast.LENGTH_SHORT).show();
+                return;
             }
         });
-
     }
-
 
     public void sign_up(View view) {
         RetrofitService retrofitService = new RetrofitService();
@@ -95,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
+                passwordET.getText().clear();
                 sendPOST(userApi, username, password);
             }
 
@@ -105,16 +109,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
     }
 
     public void sendPOST(UserApi userApi, String username, String password){
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
-
 
         userApi.saveUser(user).enqueue(new Callback<User>() {
             @Override
